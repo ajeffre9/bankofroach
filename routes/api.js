3574763e1135cf548d94e55cf53a6dcc65e953be
ajeffre9 <jeffrey.anwar1@gmail.com>
2016-09-18 05:09:47 -0400

    state

--- routes/api.js
+++ routes/api.js
@@ -29,7 +29,7 @@ body = req.body;// JSON.parse(req.body);
 
 
 	        // Insert customer
-	        client.query("INSERT INTO koala.files (username, filename, location, url, data) VALUES($1, $2, $3, $4, $5);", [data.username, data.filename, data.location, data.url, data.type], function (err, result) {
+	        client.query("INSERT INTO koala.files (username, filename, location, url, type) VALUES($1, $2, $3, $4, $5);", [data.username, data.filename, data.location, data.url, data.type], function (err, result) {
 				done();
 				res.send();
 
